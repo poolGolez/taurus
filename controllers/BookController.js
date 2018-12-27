7094ddc724b4468aaafd3226e40a64170184fe91
poolGolez <loop.edward@gmail.com>
2018-12-27 21:13:16 +0800

    Add endpoint  to save book

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -5,11 +5,25 @@ function BookController() {
 }
 
 BookController.prototype.buildApi = function(app) {
-    app.get('/book', listBooks(this.bookRepository));
+    app.route('/books')
+        .get(listBooks(this.bookRepository))
+        .post(saveBook(this.bookRepository))
+    ;
+
+
+    function saveBook(repository) {
+        return async function(request, response) {
+            const bookProperties = request.body;
+            var book = await repository.save(bookProperties);
+
+            response.statusCode = 201;
+            response.end(JSON.stringify(book));
+        }
+    }
 
     function listBooks(repository) {
-        return function (request, response) {
-            var books =  repository.findAll();
+        return async function (request, response) {
+            var books =  await repository.findAll();
             response.set('Content-Type', 'application/json');
             response.end(JSON.stringify(books));
         };
