75c2427986d021f9ab8e43b6258a9415dc59baa4
poolGolez <loop.edward@gmail.com>
2018-12-27 21:12:33 +0800

    Add methods save and findAll as async functions

--- repository/BookRepository.js
+++ repository/BookRepository.js
@@ -1,4 +1,4 @@
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 var Book = require('./../schema/book');
 
@@ -7,15 +7,35 @@ function BookRepository() {
 
 }
 
-function save(book) {
-
+async function save(properties) {
+    try {
+        mongoose.connect('mongodb://localhost:27017/taurus');
+        var db = mongoose.connection;
+        const book = new Book({
+            id: properties.id,
+            title: properties.title
+        });
+        console.log(properties);
+        await book.save();
+        db.close();
+        return book;
+    } catch(err) {
+        throw err;
+    }
 }
 
-function findAll() {
-    return [
-        new Book({ id: '541', title: 'Evolutionary Architecture' }),
-        new Book({ id: '542', title: 'Docker for Dummies' }),
-    ];
+async function findAll() {
+    try {
+        mongoose.connect('mongodb://localhost:27017/taurus');
+        var db = mongoose.connection;
+
+        var books = await Book.find({}).exec();
+        db.close();
+
+        return books;
+    } catch(err) {
+        throw err;
+    }
 }
 
 BookRepository.prototype.save = save;
